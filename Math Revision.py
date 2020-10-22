aaa66356e4f289ef0b3dea6567838c1c591947be
Xuan-wei0 <69779069+Xuan-wei0@users.noreply.github.com>
2020-10-22 13:51:32 +0800

    Update Math Revision.py

--- Math Revision.py
+++ Math Revision.py
@@ -4,23 +4,24 @@ print("Welcome to this wonderful math revision website! Please answer the follow
 print("Please respond with an answer")
 print()
 
-nultiplication1 = input("what is the answer for 23x3?")
+Question1 = input("what is the answer for 23x3?")
 if text1 = input(69)
   print("Great!")
 else:
   print("Oops! Try again")
 
-multiplication2 = input("what is the answer for 23x5-(70-1)?")
+Question2 = input("what is the answer for 23x5-(70-1)?")
 if text2 = imput(46)
   print("Great Job!")
 else("Oops! Try again")
 
-multiplication3(final) = input("what is the answer for 34-67+689+(69x3)?")
+Question3 = input("what is the answer for 34-67+689+(69x3)?")
 if text3 = imput(863)
   print("Yay! You got it correct!")
 else:
   print("Oops! Try again") 
-  
-mathfinal = int(text1)+ int(text2)
+
+Question4 = input("What is the answer for 68x2-(88/4+2)+995? /means divide")  
+if text  
 if text1 > text2 > text3(final):
   print("Hooray! You did it!")
