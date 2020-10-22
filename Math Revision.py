97ea7ef3ee900dd53753367089781a708c0a0ce5
Kiphus <69779206+Kiphus@users.noreply.github.com>
2020-10-22 13:59:35 +0800

    Update Math Revision.py

--- Math Revision.py
+++ Math Revision.py
@@ -11,17 +11,23 @@ else:
   print("Oops! Try again")
 
 Question2 = input("what is the answer for 23x5-(70-1)?")
-if text2 = imput(46)
+if text2 = input(46)
   print("Great Job!")
-else("Oops! Try again")
+else:
+  print("Oops! Try again")
 
 Question3 = input("what is the answer for 34-67+689+(69x3)?")
-if text3 = imput(863)
+if text3 = input(863)
   print("Yay! You got it correct!")
 else:
   print("Oops! Try again") 
 
 Question4 = input("What is the answer for 68x2-(88/4+2)+995? /means divide")  
 if text  
-if text1 > text2 > text3(final):
+
+Question5 = input("What is the answer for 69+111+(629x4)?")
+if text5 = input(2696)
   print("Hooray! You did it!")
+else:
+  print("Oops! Try again") 
+  
